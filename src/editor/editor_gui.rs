1f0eb40dcd4f932c02707a153fae54845c9445ab
Ayoub Gharbi <contact@ayoub-gharbi.org>
2024-05-11 20:10:33 -0400

    Added an entity spawn and despawn to clean up previously spawned sprite sheet.

--- src/editor/editor_gui.rs
+++ src/editor/editor_gui.rs
@@ -1,26 +1,37 @@
 use bevy::app::{App, Plugin};
-use bevy::prelude::{Res, Update};
+use bevy::prelude::{Res, ResMut, Resource, Update};
 use bevy::utils::petgraph::visit::Walker;
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 use crate::editor::editor_sprite_sheet::SpriteSheets;
+
+#[derive(Resource, Default)]
+pub struct SelectedSpriteSheet {
+    pub id: Option<String>,
+}
 
 pub struct EditorGuiPlugin;
 
 impl Plugin for EditorGuiPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(EguiPlugin)
+            .insert_resource(SelectedSpriteSheet::default())
             .add_systems(Update, load_sprite_sheets);
     }
 }
 
-fn load_sprite_sheets(mut egui_contexts: EguiContexts, sprite_sheets: Res<SpriteSheets>) {
+fn load_sprite_sheets(
+    mut egui_contexts: EguiContexts,
+    sprite_sheets: Res<SpriteSheets>,
+    mut selected_sprite_sheet: ResMut<SelectedSpriteSheet>, ) {
     let ctx = egui_contexts.ctx_mut();
 
     egui::SidePanel::left("side_panel")
         .default_width(200.0)
         .resizable(true).show(ctx, |ui| {
         for (id, sprite_sheet_atlas) in sprite_sheets.sheets.iter() {
-            if (ui.button(id)).clicked() {}
+            if (ui.button(id)).clicked() {
+                selected_sprite_sheet.id = Some(id.clone());
+            }
         }
     });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod editor;
+
 use bevy::app::{App};
 use bevy::DefaultPlugins;
 use bevy::prelude::*;
 use bevy::utils::petgraph::visit::Walker;
-use crate::editor::editor_gui::EditorGuiPlugin;
+use crate::editor::editor_gui::{EditorGuiPlugin, SelectedSpriteSheet};
 use crate::editor::editor_sprite_sheet::{EditorSpriteSheetPlugin, SpriteSheets};
+
+#[derive(Default, Resource)]
+struct CurrentSpriteSheetEntity {
+    pub entity: Option<Entity>,
+}
+
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
         .add_plugins(EditorSpriteSheetPlugin)
         .add_plugins(EditorGuiPlugin)
+        .insert_resource(CurrentSpriteSheetEntity::default())
         .add_systems(Startup, spawn_camera)
-        .add_systems(Startup, load_sprite_sheet)
+        .add_systems(Update, display_selected_sprite_sheet)
         .run();
 }
 
@@ -20,30 +28,38 @@ fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 
-fn load_sprite_sheet(
+fn display_selected_sprite_sheet(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     sprite_sheets: Res<SpriteSheets>,
+    selected_sprite_sheet: Res<SelectedSpriteSheet>,
     texture_atlases: Res<Assets<TextureAtlasLayout>>,
+    mut current_sprite_sheet_entity: ResMut<CurrentSpriteSheetEntity>,
 ) {
-    if let Some((_, sprite_sheet_atlas)) = sprite_sheets.sheets.iter().next() {
-        // Load the texture associated with the image path
-        let texture_handle: Handle<Image> = asset_server.load(&sprite_sheet_atlas.image_path);
 
-        // Here we assume the atlas layout is already loaded into texture_atlases,
-        // and we simply reference it in the TextureAtlas component
-        if let Some(layout) = texture_atlases.get(&sprite_sheet_atlas.handle) {
-            // Now, we need to create a TextureAtlas from the layout to use in the bundle
-            // Assuming TextureAtlas can directly use TextureAtlasLayout (if not, adjust accordingly)
-            commands.spawn(SpriteSheetBundle {
-                texture: texture_handle,
-                atlas: TextureAtlas {
-                    layout: sprite_sheet_atlas.handle.clone(),
-                    index: 0,  // Starting index
-                },
-                transform: Transform::from_scale(Vec3::splat(6.0)),
-                ..default()
-            });
+    // Clear the previous sprite sheet entity if it exists
+    if let Some(entity) = current_sprite_sheet_entity.entity {
+        commands.entity(entity).despawn();
+    }
+
+    if let Some(id) = &selected_sprite_sheet.id {
+        if let Some(sprite_sheet_atlas) = sprite_sheets.sheets.get(id) {
+            let texture_handle: Handle<Image> = asset_server.load(&sprite_sheet_atlas.image_path);
+            // Assuming the layout is already loaded and ready to be used
+            if let Some(layout) = texture_atlases.get(&sprite_sheet_atlas.handle) {
+                let entity = commands.spawn(SpriteSheetBundle {
+                    texture: texture_handle,
+                    atlas: TextureAtlas {
+                        layout: sprite_sheet_atlas.handle.clone(),
+                        index: 0,
+                    },
+                    transform: Transform::from_scale(Vec3::splat(6.0)),
+                    ..default()
+                }).id();
+
+                // Update the resource with the new entity
+                current_sprite_sheet_entity.entity = Some(entity);
+            }
         }
     }
 }
